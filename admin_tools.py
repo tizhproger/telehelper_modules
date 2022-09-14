efb1161d7a740356c6333dc946e11eb7ca5b703d
tizhproger <35668044+tizhproger@users.noreply.github.com>
2022-09-14 04:58:18 +0300

    Add files via upload

---
+++ admin_tools.py
@@ -0,0 +1,254 @@
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import base_loader, utils, security
+import logging
+
+from telethon.tl.types import ChatAdminRights, ChatBannedRights, PeerUser, PeerChannel
+from telethon.errors import BadRequestError
+from telethon.tl.functions.channels import EditAdminRequest, EditBannedRequest
+from telethon.tl.functions.messages import EditChatAdminRequest
+
+logger = logging.getLogger(__name__)
+
+
+@base_loader.tds
+class BanMod(base_loader.Module):
+    """Задачи администрирования группой"""
+    strings = {"name": "Администрирование",
+               "ban_not_supergroup": "<b>Я не могу забанить кого-то, если он не в супергруппе!</b>",
+               "unban_not_supergroup": "<b>Я не могу снять бан с кого-либо, если он не забанен в супергруппе!</b>",
+               "kick_not_group": "<b>Я не могу исключить кого-то, если он не в группе!</b>",
+               "mute_not_supergroup": "<b>Я не могу заглушить кого-то, если он не в супергруппе!</b>",
+               "unmute_not_supergroup": "<b>Я не могу вернуть голос кому-то, если он не в супергруппе!</b>",
+               "ban_none": "<b>Я не могу никого забанить, не так ли?</b>",
+               "unban_none": "<b>Мне нужен кто-то, чтобы разбанить его.</b>",
+               "kick_none": "<b>Мне нужен кто-то, чтобы исключить его из чата.</b>",
+               "promote_none": "<b>Кому мне повысить права?</b>",
+               "demote_none": "<b>Кому мне понизить права?</b>",
+               "mute_none": "<b>Кого мне заглушить?</b>",
+               "unmute_none": "<b>С кого мне снять мут?</b>",
+               "who": "<b>Кто это, черт возьми?</b>",
+               "not_admin": "<b>Разве я админ здесь?</b>",
+               "banned": "<code>{}</code> <b>забанен в чате!</b>",
+               "unbanned": "<code>{}</code> <b>разбанен в чате!</b>",
+               "kicked": "<code>{}</code> <b>исключён из чата!</b>",
+               "promoted": "<code>{}</code> <b>теперь с правами администратора!</b>",
+               "demoted": "<code>{}</code> <b>теперь без прав администратора!</b>",
+               "muted": "<code>{}</code> <b>заглушён!</b>",
+               "unmuted": "С <code>{}</code> <b>снят мут!</b>"}
+
+    @base_loader.group_admin_ban_users
+    @base_loader.ratelimit
+    async def bancmd(self, message):
+        """Забанить пользователя в группе."""
+        if not isinstance(message.to_id, PeerChannel):
+            return await utils.answer(message, self.strings("ban_not_supergroup", message))
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if len(args) == 0:
+                return await utils.answer(message, self.strings("ban_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        logger.debug(user)
+        try:
+            await self.client(EditBannedRequest(message.chat_id, user.id,
+                                                ChatBannedRights(until_date=None, view_messages=True)))
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("ban", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message,
+                               self.strings("banned", message).format(utils.escape_html(ascii(user.first_name))))
+
+    @base_loader.group_admin_ban_users
+    @base_loader.ratelimit
+    async def unbancmd(self, message):
+        """Снять бан с пользователя."""
+        if not isinstance(message.to_id, PeerChannel):
+            return await utils.answer(message, self.strings("not_supergroup", message))
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if len(args) == 0:
+                return await utils.answer(message, self.strings("unban_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        logger.debug(user)
+        try:
+            await self.client(EditBannedRequest(message.chat_id, user.id,
+                              ChatBannedRights(until_date=None, view_messages=False)))
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("unban", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message,
+                               self.strings("unbanned", message).format(utils.escape_html(ascii(user.first_name))))
+
+    @base_loader.group_admin_ban_users
+    @base_loader.ratelimit
+    async def kickcmd(self, message):
+        """Исключить пользователя из группы."""
+        if isinstance(message.to_id, PeerUser):
+            return await utils.answer(message, self.strings("not_group", message))
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if len(args) == 0:
+                return await utils.answer(message, self.strings("kick_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        logger.debug(user)
+        if user.is_self:
+            if not (await message.client.is_bot()
+                    or await self.allmodules.check_security(message, security.OWNER | security.SUDO)):
+                return
+        try:
+            await self.client.kick_participant(message.chat_id, user.id)
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("kick", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message,
+                               self.strings("kicked", message).format(utils.escape_html(ascii(user.first_name))))
+
+    @base_loader.group_admin_add_admins
+    @base_loader.ratelimit
+    async def promotecmd(self, message):
+        """Предоставляет права администратора указанному пользователю."""
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if not args:
+                return await utils.answer(message, self.strings("promote_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        rank = ""
+        if len(args) >= 1:
+            rank = args[1]
+        logger.debug(user)
+        try:
+            if message.is_channel:
+                await self.client(EditAdminRequest(message.chat_id, user.id,
+                                                   ChatAdminRights(post_messages=None,
+                                                                   add_admins=None,
+                                                                   invite_users=None,
+                                                                   change_info=None,
+                                                                   ban_users=None,
+                                                                   delete_messages=True,
+                                                                   pin_messages=True,
+                                                                   edit_messages=None), rank))
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("promote", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message,
+                               self.strings("promoted", message).format(utils.escape_html(ascii(user.first_name))))
+
+    @base_loader.group_admin_add_admins
+    async def demotecmd(self, message):
+        """Лишает прав администратора указанному пользователю."""
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if len(args) == 0:
+                return await utils.answer(message, self.strings("demote_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        logger.debug(user)
+        try:
+            if message.is_channel:
+                await self.client(EditAdminRequest(message.chat_id, user.id,
+                                                   ChatAdminRights(post_messages=None,
+                                                                   add_admins=None,
+                                                                   invite_users=None,
+                                                                   change_info=None,
+                                                                   ban_users=None,
+                                                                   delete_messages=None,
+                                                                   pin_messages=None,
+                                                                   edit_messages=None), ""))
+            else:
+                await self.client(EditChatAdminRequest(message.chat_id, user.id, False))
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("demote", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message,
+                               self.strings("demoted", message).format(utils.escape_html(ascii(user.first_name))))
+
+    async def mutecmd(self, message):
+        """Заглушает пользователя в группе."""
+        if not isinstance(message.to_id, PeerChannel):
+            return await utils.answer(message, self.strings("mute_not_supergroup", message))
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if len(args) == 0:
+                return await utils.answer(message, self.strings("mute_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        logger.debug(user)
+        try:
+            await self.client(EditBannedRequest(message.chat_id, user.id,
+                                                ChatBannedRights(until_date=None, send_messages=True)))
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("mute", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message, self.strings("muted",
+                                                     message).format(utils.escape_html(ascii(user.first_name))))
+
+    async def unmutecmd(self, message):
+        """Снимает мут с пользователя"""
+        if not isinstance(message.to_id, PeerChannel):
+            return await utils.answer(message, self.strings("unmute_not_supergroup", message))
+        if message.is_reply:
+            user = await utils.get_user(await message.get_reply_message())
+        else:
+            args = utils.get_args(message)
+            if len(args) == 0:
+                return await utils.answer(message, self.strings("unmute_none", message))
+            user = await self.client.get_entity(args[0])
+        if not user:
+            return await utils.answer(message, self.strings("who", message))
+        logger.debug(user)
+        try:
+            await self.client(EditBannedRequest(message.chat_id, user.id, ChatBannedRights(until_date=None,
+                                                view_messages=None, send_messages=False, send_media=False,
+                                                send_stickers=False, send_gifs=False, send_games=False,
+                                                send_inline=False, embed_links=False)))
+        except BadRequestError:
+            await utils.answer(message, self.strings("not_admin", message))
+        else:
+            await self.allmodules.log("unmute", group=message.chat_id, affected_uids=[user.id])
+            await utils.answer(message, self.strings("unmuted",
+                                                     message).format(utils.escape_html(ascii(user.first_name))))
+
+    async def client_ready(self, client, db):
+        self.client = client
---
+++ autoprofile.py
@@ -0,0 +1,226 @@
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import asyncio
+import ast
+import time
+import logging
+from io import BytesIO
+from telethon.tl import functions
+import base_loader, utils
+
+logger = logging.getLogger(__name__)
+
+try:
+    from PIL import Image
+except ImportError:
+    pil_installed = False
+else:
+    pil_installed = True
+
+
+@base_loader.tds
+class AutoProfileMod(base_loader.Module):
+    """Автоматическое управление вашим профилем :P"""
+    strings = {"name": "Autoprofile",
+               "missing_pil": "<b>У вас не установлен модуль Pillow</b>",
+               "missing_pfp": "<b>У вас нет изображения профиля для вращения</b>",
+               "invalid_args": "<b>Отсутствуют параметры, пожалуйста, прочитайте документацию </b>",
+               "invalid_degrees": "<b>Неверное значение градусов, пожалуйста, прочитайте документацию</b>",
+               "invalid_delete": "<b>Пожалуйста, укажите, удалять старые фотографии или нет</b>",
+               "enabled_pfp": "<b>Включен поворот изображения профиля</b>",
+               "pfp_not_enabled": "<b>Вращение изображения профиля не включено</b>",
+               "pfp_disabled": "<b>Поворот изображения профиля отключен</b>",
+               "missing_time": "<b>Время в био не указано</b>",
+               "enabled_bio": "<b>Часы в био включены</b>",
+               "bio_not_enabled": "<b>Часы в био не включены</b>",
+               "disabled_bio": "<b>Часы в био выключены</b>",
+               "enabled_name": "<b>Включены часы в имени</b>",
+               "name_not_enabled": "<b>Часы в имени не включены</b>",
+               "disabled_name": "<b>Часы в имени выключены</b>",
+               "how_many_pfps": "<b>Пожалуйста, укажите, сколько фотографий профиля должно быть удалено</b>",
+               "invalid_pfp_count": "<b>Неверное количество фотографий профиля для удаления</b>",
+               "removed_pfps": "<b>Удалено {} фотографий профиля</b>"}
+
+    def __init__(self):
+        self.bio_enabled = False
+        self.name_enabled = False
+        self.pfp_enabled = False
+        self.raw_bio = None
+        self.raw_name = None
+
+    async def client_ready(self, client, db):
+        self.client = client
+
+    async def autopfpcmd(self, message):
+        """Поворот изображения профиля (аватарки) каждые 60 секунд на x градусов, использование:
+           .autopfp <градусы> <True/1/False/0>
+
+           Градусы - 60, -10, и т.д.
+           Удаление последнего изображения - True/1/False/0, чувствительный к регистру"""
+
+        if not pil_installed:
+            return await utils.answer(message, self.strings("missing_pil", message))
+
+        if not await self.client.get_profile_photos("me", limit=1):
+            return await utils.answer(message, self.strings("missing_pfp", message))
+
+        msg = utils.get_args(message)
+        if len(msg) != 2:
+            return await utils.answer(message, self.strings("invalid_args", message))
+
+        try:
+            degrees = int(msg[0])
+        except ValueError:
+            return await utils.answer(message, self.strings("invalid_degrees", message))
+
+        try:
+            delete_previous = ast.literal_eval(msg[1])
+        except (ValueError, SyntaxError):
+            return await utils.answer(message, self.strings("invalid_delete", message))
+
+        with BytesIO() as pfp:
+            await self.client.download_profile_photo("me", file=pfp)
+            raw_pfp = Image.open(pfp)
+
+            self.pfp_enabled = True
+            pfp_degree = 0
+            await self.allmodules.log("start_autopfp")
+            await utils.answer(message, self.strings("enabled_pfp", message))
+
+            while self.pfp_enabled:
+                pfp_degree = (pfp_degree + degrees) % 360
+                rotated = raw_pfp.rotate(pfp_degree)
+                with BytesIO() as buf:
+                    rotated.save(buf, format="JPEG")
+                    buf.seek(0)
+
+                    if delete_previous:
+                        await self.client(functions.photos.
+                                          DeletePhotosRequest(await self.client.get_profile_photos("me", limit=1)))
+
+                    await self.client(functions.photos.UploadProfilePhotoRequest(await self.client.upload_file(buf)))
+                    buf.close()
+                await asyncio.sleep(60)
+
+    async def stopautopfpcmd(self, message):
+        """Остановка вращения аватарки"""
+
+        if self.pfp_enabled is False:
+            return await utils.answer(message, self.strings("pfp_not_enabled", message))
+        else:
+            self.pfp_enabled = False
+
+            await self.client(functions.photos.DeletePhotosRequest(
+                await self.client.get_profile_photos("me", limit=1)
+            ))
+            await self.allmodules.log("stop_autopfp")
+            await utils.answer(message, self.strings("pfp_disabled", message))
+
+    async def autobiocmd(self, message):
+        """Автоматически изменяет биографию вашего аккаунта в зависимости от текущего времени и использование:
+            .autobio '<сообщение, время как {time}>'"""
+
+        msg = utils.get_args(message)
+        if len(msg) != 1:
+            return await utils.answer(message, self.strings("invalid_args", message))
+        raw_bio = msg[0]
+        if "{time}" not in raw_bio:
+            return await utils.answer(message, self.strings("missing_time", message))
+
+        self.bio_enabled = True
+        self.raw_bio = raw_bio
+        await self.allmodules.log("start_autobio")
+        await utils.answer(message, self.strings("enabled_bio", message))
+
+        while self.bio_enabled is True:
+            current_time = time.strftime("%H:%M")
+            bio = raw_bio.format(time=current_time)
+            await self.client(functions.account.UpdateProfileRequest(
+                about=bio
+            ))
+            await asyncio.sleep(60)
+
+    async def stopautobiocmd(self, message):
+        """Остановка автобио"""
+
+        if self.bio_enabled is False:
+            return await utils.answer(message, self.strings("bio_not_enabled", message))
+        else:
+            self.bio_enabled = False
+            await self.allmodules.log("stop_autobio")
+            await utils.answer(message, self.strings("disabled_bio", message))
+            await self.client(functions.account.UpdateProfileRequest(about=self.raw_bio.format(time="")))
+
+    async def autonamecmd(self, message):
+        """Автоматически меняет ваше имя в Telegram с текущим временем, использование:
+            .autoname '<сообщение, время как {time}>'"""
+
+        msg = utils.get_args(message)
+        if len(msg) != 1:
+            return await utils.answer(message, self.strings("invalid_args", message))
+        raw_name = msg[0]
+        if "{time}" not in raw_name:
+            return await utils.answer(message, self.strings("missing_time", message))
+
+        self.name_enabled = True
+        self.raw_name = raw_name
+        await self.allmodules.log("start_autoname")
+        await utils.answer(message, self.strings("enabled_name", message))
+
+        while self.name_enabled is True:
+            current_time = time.strftime("%H:%M")
+            name = raw_name.format(time=current_time)
+            await self.client(functions.account.UpdateProfileRequest(
+                first_name=name
+            ))
+            await asyncio.sleep(60)
+
+    async def stopautonamecmd(self, message):
+        """Остановка часов в имени"""
+
+        if self.name_enabled is False:
+            return await utils.answer(message, self.strings("name_not_enabled", message))
+        else:
+            self.name_enabled = False
+            await self.allmodules.log("stop_autoname")
+            await utils.answer(message, self.strings("disabled_name", message))
+            await self.client(functions.account.UpdateProfileRequest(
+                first_name=self.raw_name.format(time="")
+            ))
+
+    async def delpfpcmd(self, message):
+        """Удалить n-ное количество фото профиля из вашего профиля.
+        .delpfp <pfps count/unlimited - удалить все>"""
+
+        args = utils.get_args(message)
+        if not args:
+            return await utils.answer(message, self.strings("how_many_pfps", message))
+        try:
+            pfps_count = int(args[0])
+        except ValueError:
+            return await utils.answer(message, self.strings("invalid_pfp_count", message))
+        if pfps_count < 0:
+            return await utils.answer(message, self.strings("invalid_pfp_count", message))
+        if pfps_count == 0:
+            pfps_count = None
+
+        to_delete = await self.client.get_profile_photos("me", limit=pfps_count)
+        await self.client(functions.photos.DeletePhotosRequest(to_delete))
+
+        await self.allmodules.log("delpfp")
+        await utils.answer(message, self.strings("removed_pfps", message).format(len(to_delete)))
+        return await utils.answer(message, self.strings("how_many_pfps", message))
