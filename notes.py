2022858de662b2234f529e7ced9b1029c4c21c05
tizhproger <35668044+tizhproger@users.noreply.github.com>
2022-09-14 05:09:07 +0300

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,116 @@
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+
+from .. import base_loader, utils
+
+logger = logging.getLogger(__name__)
+
+
+@base_loader.tds
+class NotesMod(base_loader.Module):
+    """Хранит глобальные заметки (или же отрывки)"""
+    strings = {"name": "Notes",
+               "what_note": "<b>Какую заметку хотите получить?</b>",
+               "no_note": "<b>Заметка не найдена</b>",
+               "save_what": "<b>Вы должны ответить на сообщение, чтобы сохранить его в заметки, или напишите заметку.</b>",
+               "what_name": "<b>Вы должны указать как заметка должна называться.</b>",
+               "saved": "<b>Заметка сохранена</b>",
+               "notes_header": "<b>Сохранённые заметки:</b>\n\n",
+               "notes_item": "<b>➤</b> <code>{}</code>",
+               "delnote_args": "<b>Какую заметку следует удалить?</b>",
+               "delnote_done": "<b>Заметка удалена</b>",
+               "delnotes_none": "<b>Нет заметок для очистки</b>",
+               "delnotes_done": "<b>Все заметки очищены</b>",
+               "notes_none": "<b>Сохраненные заметки отсутствуют</b>"}
+
+    async def notecmd(self, message):
+        """Получает указанную заметку"""
+        args = utils.get_args(message)
+        if not args:
+            await utils.answer(message, self.strings("what_note", message))
+            return
+        asset_id = self._db.get(__name__, "notes", {}).get(args[0], None)
+        logger.debug(asset_id)
+        if asset_id is not None:
+            asset = await self._db.fetch_asset(asset_id)
+        else:
+            asset = None
+        if asset is None:
+            self.del_note(args[0])
+            await utils.answer(message, self.strings("no_note", message))
+            return
+
+        await utils.answer(message, await self._db.fetch_asset(asset_id))
+
+    async def delallnotescmd(self, message):
+        """Удаляет все сохраненные заметки"""
+        if not self._db.get(__name__, "notes", {}):
+            await utils.answer(message, self.strings("delnotes_none", message))
+            return
+        self._db.get(__name__, "notes", {}).clear()
+        await utils.answer(message, self.strings("delnotes_done", message))
+
+    async def savecmd(self, message):
+        """Сохранить новую заметку. Должен использоваться в ответе с одним параметром (именем заметки)"""
+        args = utils.get_args(message)
+        if not args:
+            await utils.answer(message, self.strings("what_name", message))
+            return
+        if not message.is_reply:
+            if len(args) < 2:
+                await utils.answer(message, self.strings("save_what", message))
+                return
+            else:
+                message.entities = None
+                message.message = args[1]
+                target = message
+                logger.debug(target.message)
+        else:
+            target = await message.get_reply_message()
+        asset_id = await self._db.store_asset(target)
+        self._db.set(__name__, "notes", {**self._db.get(__name__, "notes", {}), args[0]: asset_id})
+        await utils.answer(message, self.strings("saved", message))
+
+    async def delnotecmd(self, message):
+        """Удаляет заметку, указанную по соответствующему имени"""
+        args = utils.get_args(message)
+        if not args:
+            await utils.answer(message, self.strings("delnote_args", message))
+        self.del_note(args[0])
+        await utils.answer(message, self.strings("delnote_done", message))
+
+    def del_note(self, note):
+        old = self._db.get(__name__, "notes", {})
+        try:
+            del old[note]
+        except KeyError:
+            pass
+        else:
+            self._db.set(__name__, "notes", old)
+
+    async def notescmd(self, message):
+        """Список сохраненных заметок"""
+        if not self._db.get(__name__, "notes", {}):
+            await utils.answer(message, self.strings("notes_none", message))
+            return
+        await utils.answer(message, self.strings("notes_header", message)
+                           + "\n".join(self.strings("notes_item", message).format(key)
+                           for key in self._db.get(__name__, "notes", {})))
+
+    async def client_ready(self, client, db):
+        self._db = db
---
+++ recentactions.py
@@ -0,0 +1,69 @@
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import telethon
+
+import base_loader, utils
+
+
+@base_loader.tds
+class RecentActionsMod(base_loader.Module):
+    """Показывает недавние действия в чате"""
+    strings = {"name": "Recent Actions",
+               "reply_start": "<b>Ответьте на сообщение, чтобы указать, с чего начать</b>",
+               "invalid_chat": "<b>Это не супергруппа или канал</b>",
+               "needs_admin": "<b>Необходимы права администратора для чтения удаленных сообщений</b>",
+               "recovered": "Удаленное сообщение {} восстановлено. Первоначально отправлено в {} пользователем {}, удалено в {} пользователем {}"}
+
+    @base_loader.group_admin
+    @base_loader.ratelimit
+    async def recoverdeletedcmd(self, message):
+        """Восстанавливает удаленные сообщения, отправленные после сообщения, на которое вы ответили (при желании укажите, сколько нужно восстановить)"""
+        msgs = message.client.iter_admin_log(message.to_id, delete=True)
+        if not message.is_reply:
+            await utils.answer(message, self.strings("reply_start", message))
+            return
+        if not isinstance(message.to_id, telethon.tl.types.PeerChannel):
+            await utils.answer(message, self.strings("invalid_chat", message))
+            return
+        target = (await message.get_reply_message()).date
+        ret = []
+        try:
+            async for msg in msgs:
+                if msg.original.date < target:
+                    break
+                if msg.original.action.message.date < target:
+                    continue
+                ret += [msg]
+        except telethon.errors.rpcerrorlist.ChatAdminRequiredError:
+            await utils.answer(message, self.strings("needs_admin", message))
+        args = utils.get_args(message)
+        if len(args) > 0:
+            try:
+                count = int(args[0])
+                ret = ret[-count:]
+            except ValueError:
+                pass
+        for msg in reversed(ret):
+            orig = msg.original.action.message
+            deldate = msg.original.date.isoformat()
+            origdate = orig.date.isoformat()
+            await message.respond(self.strings("recovered", message).format(msg.id, origdate, orig.from_id,
+                                                                            deldate, msg.user_id))
+            if isinstance(orig, telethon.tl.types.MessageService):
+                await message.respond("<b>" + utils.escape_html(orig.stringify()) + "</b>")
+            else:
+                await message.respond(orig)
